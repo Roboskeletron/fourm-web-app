08427af4256bd40e02fd2e8c4cb9f2f9bbaf513b
Ilya Tupitsyn <wwwrt287@gmail.com>
2025-01-06 16:58:34 +0300

    add endpoints

--- src/Forum/Forum.Api/Controllers/MessagesController.cs
+++ src/Forum/Forum.Api/Controllers/MessagesController.cs
@@ -1,4 +1,7 @@
-﻿using Forum.Application.Messages.Commands.DeleteMessage;
+﻿using Forum.Application.Common.Models;
+using Forum.Application.Messages.Commands.DeleteMessage;
+using Forum.Application.Messages.Commands.DislikeMessage;
+using Forum.Application.Messages.Commands.LikeMessage;
 using Forum.Application.Messages.Commands.UpdateMessage;
 using Microsoft.AspNetCore.Mvc;
 
@@ -31,5 +34,23 @@ public class MessagesController : ApiControllerBase
         await Mediator.Send(command, cancellationToken);
 
         return NoContent();
+    }
+
+    [HttpPatch("{id}/like")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesDefaultResponseType]
+    public async Task<ActionResult<MessageDto>> LikeMessageAsync([FromRoute] Guid id, CancellationToken cancellationToken)
+    {
+        return await Mediator.Send(new LikeMessageCommand { MessageId = id }, cancellationToken);
+    }
+
+    [HttpPatch("{id}/dislike")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesDefaultResponseType]
+    public async Task<ActionResult<MessageDto>> DislikeMessageAsync([FromRoute] Guid id, CancellationToken cancellationToken)
+    {
+        return await Mediator.Send(new DislikeMessageCommand { MessageId = id }, cancellationToken);
     }
 }
